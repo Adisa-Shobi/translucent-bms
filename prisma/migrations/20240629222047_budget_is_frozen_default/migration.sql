cb17a6fdde2fd156e9d27fc6a55df2df43e82050
Shobi <s.oadisa.dev@gmail.com>
2024-07-16 10:21:12 +0200

    chore(migrations): removed migrations from version control

--- prisma/migrations/20240629222047_budget_is_frozen_default/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Budget_ownerId_key";
-
--- AlterTable
-ALTER TABLE "Budget" ALTER COLUMN "isFrozen" SET DEFAULT false;
--- prisma/migrations/20240713081351_add_mark_delete/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "markedDeleted" BOOLEAN NOT NULL DEFAULT false;
